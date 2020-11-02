c7693821f308b28af87fb6781c3aad956b25f05f
Hadzibajramovic Amila <ahadzibajr1@etf.unsa.ba>
2020-11-02 13:17:29 +0100

    Dodani atributi u klasu Interval

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -1,6 +1,11 @@
 package ba.unsa.etf.rpr.tutorijal02;
 
 public class Interval {
+    double pocetnaTacka;
+    double krajnjaTacka;
+    boolean pocetnaTackaPripada;
+    boolean krajnjaTackaPripada;
+
 
 
 }
